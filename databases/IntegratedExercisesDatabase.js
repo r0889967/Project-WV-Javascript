7c3f37f66ad197ad1a25309f64b5ebaee14a1d08
david <davidjiawei.wang@student.kuleuven.be>
2026-04-22 22:59:23 +0200

    eerste oefeningen toegevoegd bij integrated exercises

--- databases/IntegratedExercisesDatabase.js
+++ databases/IntegratedExercisesDatabase.js
@@ -23,7 +23,28 @@ let numberPyramid = {
     fb1:"Dit wordt ook de driehoek van Pascal genoemd."
 }
 
+let catEatMice = {
+    title:"Kat eet muizen",
+    text:["We hebben een kat en 7 muizen, bij dit spel ben je de kat en je moet alle muizen opeten om te scoren",
+    "In het begin is jouw score 1, verder weten we de volgende over de muizen",
+    "Als je de rode muis opeet, wordt jouw score vermenigvuldigd met 2.",
+    "Als je de orange muis opeet, wordt jouw score vermenigvuldigd met zichzelf.",
+    "Als je de gele muis opeet, wordt jouw score 0.",
+    "Als je de groene muis opeet, komt er 15 bij de score.",
+    "Als je de blauwe muis opeet, wordt elke cijfer van jouw score de grootste cijfer van jouw score.",
+    "Als je de paarse muis opeet, wordt jouw score vermenigvuldigd met het aantal muizen die je eerder hebt opgegeten,",
+    "(de paarse muis zelf telt niet)",
+    "Als je de roze muis opeet, wordt jouw score -100.",
+    "In welke volgorde moet je de muizen opeten om de hoogste mogelijke score te behalen?"],
+    image:"images/kat en muizen.png",
+    type:"C",
+    diff:4,
+    choices:["groene muis","gele muis","roze muis","rode muis","paarse muis","blauwe muis","oranje muis"],
+    correctOrder:["roze muis","gele muis","groene muis","rode muis","oranje muis","blauwe muis","paarse muis"],
+}
+
 
 
 integExercises.push(stoneGame);//decomposition+algoritme
 integExercises.push(numberPyramid);//patroon+algoritme
+integExercises.push(catEatMice);//abstraction+decomposition+algoritme
